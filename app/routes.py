f0a97f03760873e9d3d49c304ea2587744fdcaf3
Joe OG <jogofbashan@gmail.com>
2019-12-12 20:56:01 -0500

    Stuck on passing state info

--- app/routes.py
+++ app/routes.py
@@ -7,12 +7,99 @@ import requests
 import json
 import urllib.request
 import time
+from sqlalchemy import or_
 
 @app.route('/')
 def index():
     return "Hello Joe!"
 
 @app.route('/api/test', methods=['GET'])
+def getInfo():
+    id=request.headers.get('team_id')
+    date=datetime.now()
+    output={}
+    teamResults = Team.query.filter_by(team_id=id).all()
+
+    scoreResults=Score.query.filter(Score.date<date).filter(or_(Score.home_team_id==id, Score.away_team_id==id)).filter(or_(Score.home_team_score>0, Score.away_team_score>0)).order_by(Score.date.desc()).limit(1).all()
+
+
+
+    for result in teamResults:
+        output['team'] = {
+            'team_id' : result.team_id,
+            'team_abb' : result.team_abb,
+            'team_name' : result.team_name
+            }
+
+    for result in scoreResults:
+        if result.home_team_id==Score.home_team_id:
+            output['game'] = {
+                'team_score': result.home_team_score,
+                'home_team' : True,
+                'opp_team_id' : result.away_team_id,
+                'opp_team_score': result.away_team_score,
+                'date' : result.date,
+                'game_id' : result.game_id,
+            }
+        else:
+            output['game'] = {
+                'team_score': result.home_team_score,
+                'home_team' : False,
+                'opp_team_id' : result.home_team_id,
+                'opp_team_score': result.away_team_score,
+                'date' : result.date,
+                'game_id' : result.game_id,
+            }
+
+
+    if output['game']['team_score'] > output['game']['opp_team_score']:
+            output['game']['result'] = 'W'
+    else:
+            output['game']['result'] = 'L'
+
+    oppResults = Team.query.filter_by(team_id=output['game']['opp_team_id']).all()
+
+    for result in oppResults:
+        output['game']['opp_team'] = result.team_name
+
+    statResults=Stat.query.filter_by(game_id=output['game']['game_id']).join(Player, Player.player_id==Stat.player_id).filter_by(team_id=id).order_by(Stat.points.desc()).limit(1).all()
+
+    for result in statResults:
+         output['mvp'] = {
+             'player_id' : result.player_id,
+             'pts' : result.points,
+             }
+
+    playerResults=Player.query.filter_by(player_id=output['mvp']['player_id']).all()
+    for result in playerResults:
+        output['mvp']['player_name']= result.player_name
+
+    nextGameResults=Score.query.filter(Score.date>date).filter(or_(Score.home_team_id==id, Score.away_team_id==id)).order_by(Score.date.asc()).limit(1).all()
+
+    for result in nextGameResults:
+        if output['team']['team_id']==result.home_team_id:
+            output['next_game'] = {
+                'home_team' : True,
+                'next_opp_team_id' : result.away_team_id,
+                'next_game_date' : result.date,
+            }
+        else:
+            output['next_game'] = {
+                'home_team' : False,
+                'next_opp_team_id' : result.home_team_id,
+                'next_game_date' : result.date,
+            }
+    nextOppResults = Team.query.filter_by(team_id=output['next_game']['next_opp_team_id']).all()
+
+    for result in nextOppResults:
+        output['next_game']['next_opp_team'] = result.team_name
+
+
+
+    return jsonify({'code': 200, 'output': output})
+
+
+@app.route('/api/dropdown', methods=['GET'])
 def colors():
     results=[]
     results=Team.query.all()
