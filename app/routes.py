24fedf56e94bc5a267bc4aceb8b5d680da951c5a
Joe OG <jogofbashan@gmail.com>
2019-12-18 21:19:23 -0500

    Added Scheduler.

--- app/routes.py
+++ app/routes.py
@@ -8,27 +8,24 @@ import json
 import urllib.request
 import time
 from sqlalchemy import or_
+import schedule
+
+def job():
+    getScores()
+    getStats()
+    return 'Did it'
+
+schedule.every().day.at("5:00").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
 
 @app.route('/')
 def index():
-    return "Hello Joe!"
-
-@app.route('/api/dropdown', methods=['GET'])
-#populates the dropdown button for the React Webpage. The color info is passed through to alter the CSS.
-def colors():
-    results=[]
-    results=Team.query.order_by(Team.team_id).all()
-    output=[]
-    for result in results:
-        output.append({
-            'team_id' : result.team_id,
-            'team_abb' : result.team_abb,
-            'team_name' : result.team_name,
-            'color_1' : result.color_1,
-            'color_2' : result.color_2,
-            'text_color' : result.text_color
-            })
-    return jsonify({'code' : 200, 'output' : output })
+    return 'Hello Joe!'
 
 @app.route('/api/getTodaysResults', methods=['GET'])
 # Primary API call from front end to get the results for an individual team.
@@ -288,3 +285,48 @@ def getStats():
     db.session.commit()
 
     return jsonify(stats_table)
+
+#This code creates the schedule for the year. Only needed once a season.
+@app.route('/schedule')
+def getSchedule():
+    today=datetime.now()
+    next_year = date.today() + timedelta(days=365)
+
+    url=f"https://www.balldontlie.io/api/v1/games?per_page=100&start_date={today}&end_date={next_year}"
+    data = urllib.request.urlopen(url).read()
+    result = json.loads(data)
+
+
+    score_list = []
+
+    length=result['meta']['total_pages']
+
+    for i in range(1, length+1):
+        url = f"https://www.balldontlie.io/api/v1/games?page={i}&per_page=100&start_date=2019-10-22&end_date=2020-04-15"
+        data = urllib.request.urlopen(url).read()
+        result = json.loads(data)
+        for k in result['data']:
+            score_list.append({
+                'game_id': k['id'],
+                'date': k['date'][:-14],
+                'home_team_id' :  k['home_team']['id'],
+                'home_team_score' : k['home_team_score'],
+                'away_team_id' : k['visitor_team']['id'],
+                'away_team_score': k['visitor_team_score']
+            })
+
+    for nba_game in score_list:
+        game_id = nba_game['game_id']
+        date = datetime.strptime(nba_game['date'], "%Y-%m-%d")
+        home_team_id = nba_game['home_team_id']
+        home_team_score = nba_game['home_team_score']
+        away_team_id = nba_game['away_team_id']
+        away_team_score = nba_game['away_team_score']
+
+        game_night= Score(game_id=game_id, date=date, home_team_id=home_team_id, home_team_score=home_team_score, away_team_id=away_team_id, away_team_score=away_team_score)
+
+
+        db.session.add(game_night)
+    db.session.commit()
+
+    return jsonify(score_list)
